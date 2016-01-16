4b4e88d25807edfa04d0a1be24835622514b306a
James Fogarty <jayfo.github@gmail.com>
2016-01-15 23:19:30 -0800

    Migrate Fabric to docker-compose

--- fabfile.py
+++ fabfile.py
@@ -29,20 +29,21 @@ def init():
     # Docker images can quickly fill a small disk, ensure they are on our big disk
     fabric.api.run('mkdir -p scratch/docker')
     # This points docker storage at the given directory
-    # Believe it or not, things totally explode if you have a carriage return in the file
+    # Things explode if you have a carriage return in the file, so we dos2unix just in case
     fabric.api.put('init/etc/default/docker', '/etc/default/docker', use_sudo=True)
     fabric.api.sudo('dos2unix /etc/default/docker')
 
     # Install Docker
-    fabric.api.sudo('curl -sSL https://get.docker.com/ubuntu/ | sh')
+    fabric.api.sudo('curl -sSL https://get.docker.com/ | sh')
 
-    # Install Fig
-    fabric.api.sudo('curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig')
+    # Install Compose
+    fabric.api.sudo('curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose')
+    fabric.api.sudo('chmod +x /usr/local/bin/docker-compose')
 
 
 def purge_config():
     # Clear out any existing config
-    fabric.api.run('rm -rf fig')
+    fabric.api.run('rm -rf docker-compose')
     fabric.api.run('rm -rf scratch/config')
 
 
@@ -56,7 +57,7 @@ def purge_repository(name):
     #
     # e.g. fab slicer purge_repository:pyramidwork
 
-    fabric.api.run('rm -rf fig/{name}'.format(name=name))
+    fabric.api.run('rm -rf docker-compose/{name}'.format(name=name))
 
 
 def pull_repository(name, git, branch='master'):
@@ -65,16 +66,16 @@ def pull_repository(name, git, branch='master'):
     # e.g. fab slicer pull_repository:pyramidwork,https://github.com/jayfo/docker-tractdb-pyramid.git,useractions
 
     # We'll always clone from scratch, to be safe
-    fabric.api.run('rm -rf fig/{name}'.format(name=name))
+    fabric.api.run('rm -rf docker-compose/{name}'.format(name=name))
 
-    fabric.api.run('git clone {git} fig/{name}'.format(name=name, git=git))
-    fabric.api.run('cd fig/{name} && git checkout {branch}'.format(name=name, branch=branch))
+    fabric.api.run('git clone {git} docker-compose/{name}'.format(name=name, git=git))
+    fabric.api.run('cd docker-compose/{name} && git checkout {branch}'.format(name=name, branch=branch))
 
 
 def push_config():
-    # Upload our fig file
-    fabric.api.run('mkdir -p fig/')
-    fabric.api.put('fig/fig.yml', 'fig')
+    # Upload our compose file
+    fabric.api.run('mkdir -p docker-compose/')
+    fabric.api.put('docker-compose/docker-compose.yml', 'docker-compose')
 
     # Upload our config files
     fabric.api.run('mkdir -p scratch/config/')
@@ -95,20 +96,20 @@ def start(service=None):
 
     if service is None:
         # Rebuild everything
-        fabric.api.sudo('fig -f ~/fig/fig.yml build')
+        fabric.api.sudo('docker-compose -f ~/docker-compose/docker-compose.yml build')
         # And run everything
-        fabric.api.sudo('fig -f ~/fig/fig.yml up -d')
+        fabric.api.sudo('docker-compose -f ~/docker-compose/docker-compose.yml up -d')
     else:
         # Rebuild just this one service
-        fabric.api.sudo('fig -f ~/fig/fig.yml build {}'.format(service))
+        fabric.api.sudo('docker-compose -f ~/docker-compose/docker-compose.yml build {}'.format(service))
         # And run just this one service
-        fabric.api.sudo('fig -f ~/fig/fig.yml up -d --no-deps {}'.format(service))
+        fabric.api.sudo('docker-compose -f ~/docker-compose/docker-compose.yml up -d --no-deps {}'.format(service))
 
 
 def stop(service=None):
     if service is None:
         # Stop everything
-        fabric.api.sudo('fig -f ~/fig/fig.yml stop')
+        fabric.api.sudo('docker-compose -f ~/docker-compose/docker-compose.yml stop')
     else:
         # Stop this service
-        fabric.api.sudo('fig -f ~/fig/fig.yml stop {}'.format(service))
+        fabric.api.sudo('docker-compose -f ~/docker-compose/docker-compose.yml stop {}'.format(service))
